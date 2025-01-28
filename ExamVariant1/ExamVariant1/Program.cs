cd33c03a3a07db291041a3d7616b2f70b5ab5b46
saparov-codes <saparov.inbox@gmail.com>
2025-01-28 12:23:21 +0500

    Checkimg, but not created program.cs about options

--- ExamVariant1/ExamVariant1/Program.cs
+++ ExamVariant1/ExamVariant1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ExamVariant1.DataAccess.Entities;
+using ExamVariant1.Repositories;
 
 namespace ExamVariant1;
 
@@ -27,5 +28,41 @@ internal class Program
             QuentityLikes = 3,
             Description = "Yaxshi"
         };
+
+        Console.WriteLine("Welcome To The Music Area!");
+
+        Console.WriteLine("1. Add Music");
+        Console.WriteLine("2. Search By name");
+        Console.WriteLine("3. Search By author");
+        Console.WriteLine("4. Delete Music");
+        Console.WriteLine("5. See All musics list");
+
+        Console.Write("Option : ");
+        var opption = Console.ReadLine();
+
+        MusicRepository musicRepo = new MusicRepository();
+
+        musicRepo.AddMusic(music1);
+        musicRepo.AddMusic(music3);
+        musicRepo.AddMusic(music2);
+        if(opption == "1")
+        {
+            Music musicName = new Music();
+            
+            Guid id = Guid.NewGuid();
+            musicName.Id = id;
+            Console.Write("Music Name : ");
+            musicName.Name = Console.ReadLine();
+            Console.WriteLine("Author of the Music : ");
+            musicName.AuthorName = Console.ReadLine();
+
+
+            musicRepo.AddMusic(musicName);
+            
+        }
+        else if(opption == "5")
+        {
+            musicRepo.GetAllMusic().ForEach(music => Console.WriteLine( music.Name));
+        }
     }
 }
--- ExamVariant1/ExamVariant1/obj/Debug/net8.0/ExamVariant1.AssemblyInfo.cs
+++ ExamVariant1/ExamVariant1/obj/Debug/net8.0/ExamVariant1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamVariant1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0e16138436988dd8c28e507763667fd9a8bd1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamVariant1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamVariant1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
